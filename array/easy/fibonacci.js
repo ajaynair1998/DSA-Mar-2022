951dd298e0a3f50bece8437a0e66814778fe06ab
Ajay <ajay@Ajays-MacBook-Air.local>
2022-03-21 17:31:43 +0530

    add : fibonacci

--- array/easy/fibonacci.js
+++ array/easy/fibonacci.js
@@ -2,14 +2,9 @@ let fib = (n) => {
 	return recursive_fib(n);
 };
 
-function recursive_fib(n, memo = {}) {
+function recursive_fib(n, memo = { 0: 0, 1: 1 }) {
 	if (memo[n] !== undefined) {
 		return memo[n];
-	}
-	if (n == 0) {
-		return 0;
-	} else if (n == 1) {
-		return 1;
 	} else {
 		let current_fib = recursive_fib(n - 1, memo) + recursive_fib(n - 2, memo);
 		memo[n] = current_fib;
